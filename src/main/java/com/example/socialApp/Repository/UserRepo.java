0524df4099f2e2dab373418389d401a2bcdd641a
Aya Nady <ayanady22193@gmail.com>
2025-01-18 16:34:38 -0700

    Implement login and register logic

--- src/main/java/com/example/socialApp/Repository/UserRepo.java
+++ src/main/java/com/example/socialApp/Repository/UserRepo.java
@@ -1,10 +1,12 @@
 package com.example.socialApp.Repository;
 
 import com.example.socialApp.model.Users;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepo extends JpaRepository<Users, Integer> {
 
-//    @Query("select new com.example.socialApp.DTO.ProfileInfoDTO()")
+    Optional<Users> findByEmail(String email); // Find user by email
 }
